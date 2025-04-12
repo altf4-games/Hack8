f4ff9455f702a08c2b74499f9eeedb8d9838c849
Pradyum Mistry <39883432+altf4-games@users.noreply.github.com>
2025-04-12 12:11:44 +0530

    Added Unity Base Project For Chemistry Game

---
+++ BreakingBadGame/Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    private float x;
+    private float y;
+    private Vector3 rotateValue;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        y = Input.GetAxis("Mouse X");
+        x = Input.GetAxis("Mouse Y");
+        Debug.Log(x + ":" + y);
+        rotateValue = new Vector3(x, y * -1, 0);
+        transform.eulerAngles = transform.eulerAngles - rotateValue;
+    }
+}
